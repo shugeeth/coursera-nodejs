99f94add261e2569617095456b1cda047050ef90
Shugeeth Babu <meetshugeeth@gmail.com>
2021-06-10 18:42:17 +0530

    Node Callbacks and Error Handling

--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,27 @@
-exports.perimeter = (x,y) => (2*(x+y))
-exports.area = (x,y) => (x*y)
+module.exports = (x,y,callback) => {
+    if (x <= 0 || y <= 0) {
+        setTimeout(() => {
+            return callback(
+                new Error("Rectangle dimensions should be greater than zero:  l = "
+                    + x + ",  and b = " + y),
+                null
+            )
+        }, 2000)
+        
+    }
+    else{
+        setTimeout(() => {
+            return callback(
+                null,
+                {
+                    perimeter: (x,y) => (2*(x+y)),
+                    area: (x,y) => (x*y)
+                }
+            )
+        }, 2000)
+    }
+    console.log("This statement after the call to rect()");
+}
+
+
+
